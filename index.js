d82f8baa1f3d3424014e6037ae1931340712fcbd
Qa70601234 <q70700350@qq.com>
2021-08-11 11:39:50 +0800

    feat:cf

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
-
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const SingleDay = 'xxray.herokuapp.com'
+const DoubleDay = 'xxary-1.herokuapp.com'
+addEventListener(
+    "fetch", event => {
+        let nd = new Date();
+        if (nd.getDate() % 2) {
+            host = SingleDay;
+        } else {
+            host = DoubleDay;
+        }
+        let url = new URL(event.request.url);
+        url.hostname = host;
+        let request = new Request(url, event.request);
+        event.respondWith(
+            fetch(request)
+        )
+    }
+)
